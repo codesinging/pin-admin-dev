cafb97c136661a0152de06f5a69a981332951626
codesinging <codesinging@gmail.com>
2022-03-08 22:17:25 +0800

    增加模型支持函数

---
+++ src/Support/Model/BaseModel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Support\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    use ModelTraits;
+}
---
+++ src/Support/Model/ModelTraits.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Support\Model;
+
+trait ModelTraits
+{
+    use SerializeDate;
+    use Sanitize;
+    use Lister;
+    use NewInstance;
+}
